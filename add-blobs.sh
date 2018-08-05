45b37e9d75aeefc4d17fe78199db7466a7468ce8
Toshiaki Maki <tmaki@pivotal.io>
2018-08-06 02:32:51 +0900

    init

---
+++ add-blobs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DIR=`pwd`
+
+mkdir -p .downloads
+
+cd .downloads
+
+blob_download() {
+  set -eu
+  local package=$1
+  local url=$2
+  local f=$3
+  if [ ! -f ${DIR}/blobs/${package}/${f} ];then
+    curl -L -J ${url} -o ${f}
+    bosh add-blob --dir=${DIR} ${f} ${package}/${f}
+  fi
+}
+
+blob_download python3 https://www.python.org/ftp/python/3.5.6/Python-3.5.6.tgz Python-3.5.6.tgz
+
+pip download -d curator --no-binary :all: elasticsearch-curator==5.5.4
+for f in $(ls curator/*.tar.gz);do 
+  bosh add-blob --dir=${DIR} ${f} ${f}
+done
