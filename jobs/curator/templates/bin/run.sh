915c6f65b4d8be50fe85085abab02da0a2ad738c
Toshiaki Maki <tmaki@pivotal.io>
2018-08-31 12:56:56 +0900

    Fix

--- jobs/curator/templates/bin/run.sh
+++ jobs/curator/templates/bin/run.sh
@@ -4,7 +4,7 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export PATH=/var/vcap/packages/curator/bin:$PATH
 export CONFIG_DIR=/var/vcap/jobs/curator/config
-export PYTHONPATH=/var/vcap/packages/curator/lib/python3.6/site-packages/
+export PYTHONPATH=/var/vcap/packages/curator/lib/python3.5/site-packages/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/var/vcap/packages/python3/lib
 
 curator --config $CONFIG_DIR/config.yml \
